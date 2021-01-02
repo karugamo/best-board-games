525a30eb17f5b026a3a3eb0afb6d5feac2b5b24c
Finn Pauls <mbp@pauls.dev>
2021-01-02 02:28:16 +0100

    Add complexity slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Range} from 'rc-slider'
 import styled from 'styled-components'
 import games from '../games.json'
 import Game from './Game'
 import Logo from './Logo'
+import 'rc-slider/assets/index.css'
 
 export default function App() {
+  const [complexityRange, setComplexityRange] = useState([1, 3])
   return (
     <Main>
       <Logo />
+      <RangeContainer>
+        <Range
+          value={complexityRange}
+          onChange={setComplexityRange}
+          min={1}
+          max={5}
+          defaultValue={[0, 3]}
+          marks={{1: 'very easy', 5: 'very complex'}}
+          dots
+        />
+      </RangeContainer>
       <GamesContainer>
-        {games.map((game) => {
-          return <Game key={game.name} game={game} />
-        })}
+        {games
+          .filter(
+            (game) =>
+              game.weight >= complexityRange[0] &&
+              game.weight <= complexityRange[1]
+          )
+          .map((game) => {
+            return <Game key={game.name} game={game} />
+          })}
       </GamesContainer>
     </Main>
   )
 }
+
+const RangeContainer = styled.div`
+  max-width: 500px;
+  height: 50px;
+  width: 100%;
+`
 
 const Main = styled.div`
   display: flex;
