0a1118ed73d2381293dd0f32446878a661b449fa
Finn Pauls <mbp@pauls.dev>
2021-01-06 22:15:46 +0100

    Fix updating title

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {GeekGame} from '../types'
 import GameModal from './GameModal'
+import Helmet from 'react-helmet'
 
 import {navigate} from 'gatsby'
 
@@ -12,10 +13,17 @@ type AppProps = {
 
 export default function App({pageContext: {game}}: AppProps) {
   return (
-    <GameModal
-      isOpen={Boolean(game)}
-      game={game}
-      onClose={() => navigate('/')}
-    />
+    <>
+      <Helmet>
+        <title>{`Best Board Games | ${
+          game?.name ?? 'List of good board games'
+        }`}</title>
+      </Helmet>
+      <GameModal
+        isOpen={Boolean(game)}
+        game={game}
+        onClose={() => navigate('/')}
+      />
+    </>
   )
 }
