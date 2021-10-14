ea0a82bc478ca53c744c60d46e7c46798c029a03
Finn Pauls <mbp@pauls.dev>
2021-10-14 16:17:07 +0200

    Remove Emojis for Mobile

--- src/components/OverwhelminglyPositiveLink.tsx
+++ src/components/OverwhelminglyPositiveLink.tsx
@@ -4,11 +4,9 @@ import styled from 'styled-components'
 export default function BestBoardGamesLink() {
   return (
     <Container>
-      <Emoji />
       <Link target="_blank" href="https://overwhelmingly-positive.com">
         Overwhelmingly Positive Steam Games
       </Link>
-      <Emoji />
     </Container>
   )
 }
@@ -17,20 +15,11 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 16px;
+  margin: 16px;
+  text-align: center;
 `
 
 const Link = styled.a`
   color: #272e6e;
-  font-size: 40px;
-`
-
-function Emoji() {
-  return <EmojiContainer>🕹</EmojiContainer>
-}
-
-const EmojiContainer = styled.div`
-  margin: 16px;
-  line-height: 26px;
   font-size: 40px;
 `
